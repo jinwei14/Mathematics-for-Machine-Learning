318312c160eefc379d20657d4e44e9147cb1691a
jinwei14 <janviezhang14@gmail.com>
2018-10-25 06:31:25 +0100

    adding the code for the CW

---
+++ CW1/answer.py
@@ -0,0 +1,46 @@
+# -*- coding: utf-8 -*-
+
+"""
+Use this file for your answers.
+
+This file should been in the root of the repository
+(do not move it or change the file name)
+
+"""
+
+# NB this is tested on python 2.7. Watch out for integer division
+
+import numpy as np
+
+
+def grad_f1(x):
+    """
+    4 marks
+
+    :param x: input array with shape (2, )
+    :return: the gradient of f1, with shape (2, )
+    """
+    pass
+
+
+def grad_f2(x):
+    """
+    6 marks
+
+    :param x: input array with shape (2, )
+    :return: the gradient of f2, with shape (2, )
+    """
+    pass
+
+
+def grad_f3(x):
+    """
+    This question is optional. The test will still run (so you can see if you are correct by
+    looking at the testResults.txt file), but the marks are for grad_f1 and grad_f2 only.
+
+    Do not delete this function.
+
+    :param x: input array with shape (2, )
+    :return: the gradient of f3, with shape (2, )
+    """
+    pass
