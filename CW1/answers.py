0c3a9fc235e4bcf56732bfe9807bbb119863f094
jinwei14 <janviezhang14@gmail.com>
2018-10-25 11:49:28 +0100

    adding codeds

--- CW1/answers.py
+++ CW1/answers.py
@@ -9,9 +9,19 @@ This file should been in the root of the repository
 """
 
 # NB this is tested on python 2.7. Watch out for integer division
-
+import autograd.numpy as npa
 import numpy as np
+import math
+# Thinly wrapped numpy
 
+
+# Basically everything you need
+
+from autograd import grad
+
+B = np.array([[3, -1], [-1, 3]])
+a = np.array([[1], [0]])
+b = np.array([[0], [-1]])
 
 def grad_f1(x):
     """
@@ -23,14 +33,15 @@ def grad_f1(x):
 
     answer is  gradient f(x) = (I+B)x -a -b =0
     """
-    matrix_B = np.array( [[3,-1],[-1,3]] )
-    matrix_a =np.array( [[1],[0]] )
-    matrix_b = np.array( [[0],[-1]])
+
     #fx1 = 2**.dot(x)  - matrix_a + matrix_b
 
-    fx1 = 2*(np.identity(2) + matrix_B).dot(x) - matrix_a + matrix_b
-    fx1 = fx1.reshape(2,)
-    return fx1
+    fx1 = 2*(np.identity(2) + B).dot(x) - a + b
+
+    #fx1 = 2*np.matmul((np.identity(2) + matrix_B), x) - matrix_a + matrix_b
+    #fx1 = fx1.reshape(2, )
+    ans = np.array([fx1[0,0],fx1[0,1]])
+    return ans
 
 def grad_f2(x):
     """
@@ -38,8 +49,15 @@ def grad_f2(x):
 
     :param x: input array with shape (2, )
     :return: the gradient of f2, with shape (2, )
+
+    gradient f(x) = 2(x-a)Cos((x-a)T (x-a)) + 2B(x-b)
     """
-    pass
+    x=x.reshape(2,1)
+    fx = 2*(x-a)*math.cos( np.transpose(x-a).dot(x-a))+ 2*B.dot(x-b)
+    # print(fx[1])
+    # ans = np.array(fx[0],fx[1])
+    fx = fx.reshape(2,)
+    return  fx
 
 
 def grad_f3(x):
@@ -56,5 +74,7 @@ def grad_f3(x):
 
 
 
+x= np.array( [ 1,-1 ])
+#print(grad_f1(np.array( [ 1,-1 ])).shape )
 
-print(grad_f1(np.array( [ [1],[-1] ])))
+print(grad_f2(x).shape )
