8af13b5fbbb0d6e80dfb75285b6aa76c10e70f29
jinwei14 <janviezhang14@gmail.com>
2018-11-12 21:03:06 +0000

    adding codeds

---
+++ CW3/1a.py
@@ -0,0 +1,9 @@
+"""
+that lml(alpha, beta, Phi, Y)
+that returns the log marginal likelihood, and also a function grad_lml(alpha, beta, Phi, Y)
+that returns the gradient of the log marginal likelihood with respect to the vector (α,β). 2.
+The function should return a numpy vector with the gradient with respect to α in the first component and gradient
+with respect to β in the second.
+
+"""
+
