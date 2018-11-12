9eb66b61997e5f772b0f935dd142c83481cc020b
jinwei14 <janviezhang14@gmail.com>
2018-11-12 21:10:37 +0000

    code for CW2

--- CW3/1a.py
+++ CW3/1a.py
@@ -7,3 +7,30 @@ with respect to β in the second.
 
 """
 
+import numpy as np
+import matplotlib.pyplot as plt
+from numpy.linalg import inv
+
+N = 25
+X = np.reshape(np.linspace(0, 0.9, N), (N, 1))
+Y = np.cos(10*X**2) + 0.1 * np.sin(100*X)
+#K = 12  # order of the func
+listTrainning = []
+for i in np.linspace(0, 0.9, N):
+    listTrainning.append(np.cos(10*i**2) + 0.1 * np.sin(100*i))
+
+def Phi(K):
+    Psii = np.zeros((N, K + 1))
+    for i in range(N):
+        for j in range(0, K+1):
+            Psii[i][j] = X[i][0]**j
+
+    theta = np.dot(np.dot(inv(np.dot(Psii.T, Psii)), Psii.T), Y)
+    return theta
+
+def lml(alpha, beta, Phi, Y):
+    return 0
+
+
+def grad_lml(alpha, beta, Phi, Y):
+    return 0
